d8ca6fe57a98340dc796c661d2870f36c58d9d27
Dominique Hazael-Massieux <dom@w3.org>
2014-11-26 14:33:48 +0100

    typo in header name

--- index.py
+++ index.py
@@ -84,7 +84,7 @@ def refevent(event, payload):
 
 def serveRequest():
     request_method = os.environ.get('REQUEST_METHOD', "GET")
-    remote_addr = os.environ.get('HTTP_X-FORWARDED-FOR', os.environ.get('REMOTE_ADDR'))
+    remote_addr = os.environ.get('HTTP_X_FORWARDED_FOR', os.environ.get('REMOTE_ADDR'))
     # Store the IP address blocks that github uses for hook requests.
     hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
 
