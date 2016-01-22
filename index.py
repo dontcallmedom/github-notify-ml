8cf042e9ece0946c5a9fa7979eedef793e713a22
Dominique Hazael-Massieux <dom@w3.org>
2016-01-22 15:42:22 +0100

    Refactor to enable consuming other webhooks

--- index.py
+++ index.py
@@ -94,146 +94,160 @@ def refevent(event, payload, target, oauth_token):
 
 def serveRequest(config, postbody):
     request_method = os.environ.get('REQUEST_METHOD', "GET")
+    if request_method == 'GET':
+        output = "Content-Type: text/plain; charset=utf-8\n\n"
+        output += " Nothing to see here, move along ..."
+        return output
+    if request_method != 'POST':
+        return
+    if os.environ.has_key('HTTP_X_GITHUB_EVENT'):
+        return githubRequest(config,postbody)
+
+def githubRequest(config,postbody):
     remote_addr = os.environ.get('HTTP_X_FORWARDED_FOR', os.environ.get('REMOTE_ADDR'))
+
     # Store the IP address blocks that github uses for hook requests.
     hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
     output = ""
 
-    if request_method == 'GET':
-        output += "Content-Type: text/plain; charset=utf-8\n\n"
-        output += " Nothing to see here, move along ..."
+    # Check if the request is from github.com
+    for block in hook_blocks:
+        ip = ipaddress.ip_address(u'%s' % remote_addr)
+        if ipaddress.ip_address(ip) in ipaddress.ip_network(block):
+            break #the remote_addr is within the network range of github
+    else:
+        output += "Status: 403 Unrecognized IP\n"
+        output += "Content-Type: application/json\n\n"
+        output += json.dumps({'msg': 'Unrecognized IP address', 'ip': remote_addr})
         return output
-    elif request_method == 'POST':
-        # Check if the POST request if from github.com
-        for block in hook_blocks:
-            ip = ipaddress.ip_address(u'%s' % remote_addr)
-            if ipaddress.ip_address(ip) in ipaddress.ip_network(block):
-                break #the remote_addr is within the network range of github
-        else:
-            output += "Status: 403 Unrecognized IP\n"
-            output += "Content-Type: application/json\n\n"
-            output += json.dumps({'msg': 'Unrecognized IP address', 'ip': remote_addr})
-            return output
 
-        event = os.environ.get('HTTP_X_GITHUB_EVENT', None)
-        if event == "ping":
-            output += "Content-Type: application/json\n\n"
-            output += json.dumps({'msg': 'Hi!'})
-            return output
-        mls = json.loads(io.open(config['mls'], 'r').read())
-        repos = {}
-        for (ml, mlrepos) in mls.iteritems():
-            for (reponame, repoconf) in mlrepos.iteritems():
-                repoconf["email"] = {"to":ml}
-                repos[reponame] = repoconf
-        payload = json.loads(postbody)
-        repo_meta = {
+    event = os.environ.get('HTTP_X_GITHUB_EVENT', None)
+    if event == "ping":
+        output += "Content-Type: application/json\n\n"
+        output += json.dumps({'msg': 'Hi!'})
+        return output
+    mls = json.loads(io.open(config['mls'], 'r').read())
+    repos = {}
+    for (ml, mlrepos) in mls.iteritems():
+        for (reponame, repoconf) in mlrepos.iteritems():
+            repoconf["email"] = {"to":ml}
+            repos[reponame] = repoconf
+    payload = json.loads(postbody)
+    repo_meta = {
 	    'name': payload['repository'].get('name')
 	    }
-        repo_meta['owner'] = payload['repository']['owner'].get('name', payload['repository']['owner'].get('login'))
-	match = re.match(r"refs/heads/(?P<branch>.*)", payload.get('ref', ''))
-	if match:
-	    repo_meta['branch'] = match.groupdict()['branch']
+    repo_meta['owner'] = payload['repository']['owner'].get('name', payload['repository']['owner'].get('login'))
+    match = re.match(r"refs/heads/(?P<branch>.*)", payload.get('ref', ''))
+    if match:
+        repo_meta['branch'] = match.groupdict()['branch']
 
-        formatedRepoName = "{owner}/{name}".format(**repo_meta)
+    formatedRepoName = "{owner}/{name}".format(**repo_meta)
 
-        def repoMatch(reponame):
-            if (reponame.startswith("regexp:")):
-                regexp = reponame[len("regexp:"):]
+    def repoMatch(reponame):
+        if (reponame.startswith("regexp:")):
+            regexp = reponame[len("regexp:"):]
+            try:
+                return re.match(regexp, formatedRepoName) != None
+            except:
+                return False
+        else:
+            return reponame == formatedRepoName
+
+    sentMail = []
+    errors = []
+
+    if payload.has_key("action"):
+        event = event + "." + payload['action']
+
+    for ml,repos in mls.iteritems():
+        for reponame in filter(repoMatch, repos.keys()):
+            repo = repos[reponame]
+
+            if event not in repo['events'] and (not repo_meta.has_key("branch") or event not in repo.get('branches', {}).get(repo_meta['branch'], [])):
+                continue
+            if repo.has_key("eventFilter"):
+                labelTarget = payload.get("issue", payload.get("pull_request", {})).get("labels", [])
+                labelFilter = lambda x: x.get("name") == repo["eventFilter"]["label"]
+                if repo["eventFilter"]["label"]:
+                    if not labelFilter(payload.get("label", {})) and len(filter(labelFilter, labelTarget)) == 0:
+                        continue
+            try:
+                template = io.open(config["TEMPLATES_DIR"] + "/mls/" + ml + '/' + formatedRepoName + "/%s" % event).read()
+            except IOError:
                 try:
-                    return re.match(regexp, formatedRepoName) != None
-                except:
-                    return False
-            else:
-                return reponame == formatedRepoName
-
-        sentMail = []
-        errors = []
-
-        s = smtplib.SMTP(config["SMTP_HOST"])
-        if payload.has_key("action"):
-            event = event + "." + payload['action']
-
-        for ml,repos in mls.iteritems():
-            for reponame in filter(repoMatch, repos.keys()):
-                repo = repos[reponame]
-
-                if event not in repo['events'] and (not repo_meta.has_key("branch") or event not in repo.get('branches', {}).get(repo_meta['branch'], [])):
-                    continue
-                if repo.has_key("eventFilter"):
-                    labelTarget = payload.get("issue", payload.get("pull_request", {})).get("labels", [])
-                    labelFilter = lambda x: x.get("name") == repo["eventFilter"]["label"]
-                    if repo["eventFilter"]["label"]:
-                        if not labelFilter(payload.get("label", {})) and len(filter(labelFilter, labelTarget)) == 0:
-                            continue
-                try:
-                    template = io.open(config["TEMPLATES_DIR"] + "/mls/" + ml + '/' + formatedRepoName + "/%s" % event).read()
+                    template = io.open(config["TEMPLATES_DIR"] + "/mls/" + ml + "/%s" % event).read()
                 except IOError:
                     try:
-                        template = io.open(config["TEMPLATES_DIR"] + "/mls/" + ml + "/%s" % event).read()
+                        template = io.open(config["TEMPLATES_DIR"] + "/generic/%s" % event).read()
                     except IOError:
-                        try:
-                            template = io.open(config["TEMPLATES_DIR"] + "/generic/%s" % event).read()
-                        except IOError:
-                            errors.append({'msg': 'no template defined for event %s' % event})
-                            continue
-                body = pystache.render(template, payload)
-                subject, dummy, body = body.partition('\n')
-                paragraphs = body.splitlines()
-                wrapper = textwrap.TextWrapper( break_long_words=False, break_on_hyphens=False,  drop_whitespace=False)
-                body = "\n".join(map(wrapper.fill, paragraphs))
-                msg = MIMEText(body, _charset="utf-8")
-                frum = repo.get("email", {}).get("from", config["EMAIL_FROM"])
-                msgid = "<%s-%s-%s-%s>" % (event, event_id(event, payload),
-                                           event_timestamp(event, payload), frum)
-                target = "pull_request" if payload.has_key("pull_request") or payload.get("issue", {}).has_key("pull_request") else "issue"
-                (ref_event, ref_id) = refevent(event, payload, target, config.get("GH_OAUTH_TOKEN", False))
-                inreplyto = None
-                if ref_event and ref_id:
-                    inreplyto = "<%s-%s-%s-%s>" % (ref_event, ref_id,
-                                                event_timestamp(ref_event, payload),
-                                                frum)
+                        errors.append({'msg': 'no template defined for event %s' % event})
+                        continue
+            body = pystache.render(template, payload)
+            subject, dummy, body = body.partition('\n')
+            paragraphs = body.splitlines()
+            wrapper = textwrap.TextWrapper( break_long_words=False, break_on_hyphens=False,  drop_whitespace=False)
+            body = "\n".join(map(wrapper.fill, paragraphs))
+            frum = repo.get("email", {}).get("from", config["EMAIL_FROM"])
+            msgid = "<%s-%s-%s-%s>" % (event, event_id(event, payload),
+                                       event_timestamp(event, payload), frum)
+            target = "pull_request" if payload.has_key("pull_request") or payload.get("issue", {}).has_key("pull_request") else "issue"
+            (ref_event, ref_id) = refevent(event, payload, target, config.get("GH_OAUTH_TOKEN", False))
+            inreplyto = None
+            if ref_event and ref_id:
+                inreplyto = "<%s-%s-%s-%s>" % (ref_event, ref_id,
+                                            event_timestamp(ref_event, payload),
+                                            frum)
 
-                too = repo.get("email", {}).get("to").split(",")
-                headers = {}
-                frum_name = ""
-                readable_frum = email.header.Header(charset='utf8', header_name='From')
-                if config.get("GH_OAUTH_TOKEN", False):
-                    headers['Authorization']="token %s" % (config["GH_OAUTH_TOKEN"])
-                    frum_name = requests.get(payload['sender']['url'],
-                                         headers=headers
-                                         ).json()['name']
-                    if frum_name == None:
-                        frum_name = payload['sender']['login']
-                    readable_frum.append('%s via GitHub' % (frum_name))
+            too = repo.get("email", {}).get("to").split(",")
+            headers = {}
+            frum_name = ""
 
-                readable_frum.append('<%s>' % (frum), charset='us-ascii')
-                msg['From'] = readable_frum
-                msg['To'] = ",".join(too)
-                msg['Subject'] = Header(subject, 'utf-8')
-                msg['Message-ID'] = msgid
-                if inreplyto:
-                    msg['In-Reply-To'] = inreplyto
-                # from http://wordeology.com/computer/how-to-send-good-unicode-email-with-python.html
-                m = StringIO()
-                g = Generator(m, False)
-                g.flatten(msg)
-                s.sendmail(frum, too, m.getvalue())
-                sentMail.append({"to":too, "subject": subject})
-
-        s.quit()
-        if (len(sentMail)):
-            output += "Content-Type: application/json\n\n"
-            output += json.dumps({'sent': sentMail, 'errors': errors})
+            if config.get("GH_OAUTH_TOKEN", False):
+                headers['Authorization']="token %s" % (config["GH_OAUTH_TOKEN"])
+                frum_name = requests.get(payload['sender']['url'],
+                                     headers=headers
+                                     ).json()['name']
+                if frum_name == None:
+                    frum_name = payload['sender']['login']
+                frum_name = '%s via GitHub' % (frum_name)
+            sentMail.append(sendMail(config["SMTP_HOST"], body, frum, frum_name, too, subject, msgid, inreplyto))
+        if output:
+            output = "Content-Type: application/json\n\n"
+            output += json.dumps({'sent': sentMail})
             return output
         elif (len(errors)):
             output += "Status: 500 Error processing the request\n"
             output += "Content-Type: application/json\n\n"
             output += json.dumps({'errors': errors})
             return output
-        output += "Content-Type: text/plain; charset=utf-8\n\n"
-        output += 'OK'
-        return output
+
+    output += "Content-Type: text/plain; charset=utf-8\n\n"
+    output += 'OK'
+    return output
+
+def sendMail(smtp, body, from_addr, from_name, to_addr, subject, msgid=None, inreplyto=None):
+    s = smtplib.SMTP(smtp)
+    msg = MIMEText(body, _charset="utf-8")
+    readable_from = email.header.Header(charset='utf8', header_name='From')
+    readable_from.append(from_name)
+    readable_from.append('<%s>' % (from_addr), charset='us-ascii')
+    msg['From'] = readable_from
+    msg['To'] = ",".join(to_addr)
+    msg['Subject'] = Header(subject, 'utf-8')
+    if msgid:
+        msg['Message-ID'] = msgid
+    if inreplyto:
+        msg['In-Reply-To'] = inreplyto
+
+    # from http://wordeology.com/computer/how-to-send-good-unicode-email-with-python.html
+    m = StringIO()
+    g = Generator(m, False)
+    g.flatten(msg)
+    s.sendmail(from_addr, to_addr, m.getvalue())
+    sentMail = {"to":to_addr, "subject": subject}
+
+    s.quit()
+    return sentMail
 
 if __name__ == "__main__":
     config = json.loads(io.open('instance/config.json').read())
